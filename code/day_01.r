2789d8b87986e753fb3b250d4ce43be2b7adfda3
Bas Latcham <abasi.latcham@gmail.com>
2021-12-03 15:34:56 +1100

    Day one: sonar sweep part 2

--- code/day_01.r
+++ code/day_01.r
@@ -1,6 +1,7 @@
 library(tidyverse)
 
-# How many measurements are larger than the previous measurement?
+# Part 1
+## How many measurements are larger than the previous measurement?
 input <- readLines("data/input_01.txt")
 # Argh! It was read as a character!
 input <- as.numeric(input)
@@ -8,3 +9,14 @@ input <- as.numeric(input)
 # Let's practice not using dataframes
 bigger <- (input > dplyr::lag(input, n = 1L))
 sum(bigger, na.rm = TRUE)
+
+# Part 2
+## Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+library(slider)
+
+tibble(depth = input) %>%
+    mutate(
+        window_sum = slide_sum(depth, after = 2),
+        window_bigger = window_sum > lag(window_sum)
+    ) %>%
+    count(window_bigger)
